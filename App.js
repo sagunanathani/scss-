2c06a607129086a39cb10a601540e19a358e26f1
the_saguna_nathani <104073036+sagunanathani@users.noreply.github.com>
2022-08-18 10:44:45 +0530

    It's file

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import './App.css';
+// import "../../../SCSS/my-app/src/css/style.css"
+
+const App = () => {
+    return (
+        <div>
+            <h1> I AM SASS AND SCSS</h1>
+            <p>This property specifies what kind of border to display:</p>
+        </div>
+    )
+};
+
+export default App;
